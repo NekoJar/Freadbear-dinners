f831e6458a01f35fff4083946a58f005ae487a81
NekoJar <mzharfanhakim7@gmail.com>
2023-09-12 15:02:56 +0700

    adding a flexboxes

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Jar</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jar</div>;
 }
 
 export default Username;
