231c814b04565a9457a2ffc326540107f040b233
NekoJar <mzharfanhakim7@gmail.com>
2023-09-14 15:30:43 +0700

    buliding the cart page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -26,7 +26,7 @@ const cartSlice = createSlice({
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
     },
-    clearCart(state, action) {
+    clearCart(state) {
       state.cart = [];
     },
   },
@@ -41,6 +41,8 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
 
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
