27e0d2564974524b0ce0f3f8481f1680a73af5b4
NekoJar <mzharfanhakim7@gmail.com>
2023-09-14 15:52:54 +0700

    deleting cart items

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,11 +1,8 @@
-import { useDispatch } from "react-redux";
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { deleteItem } from "./cartSlice";
+
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
-  const dispatch = useDispatch();
-
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
@@ -15,9 +12,8 @@ function CartItem({ item }) {
       </p>
       <div className=" flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
-          Delete
-        </Button>
+
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
